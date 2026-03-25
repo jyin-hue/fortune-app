12972fefd41f24515fca8591da198dedfe28a78b
jyin-hue <jy.in@medibuilder.com>
2026-03-25 09:34:13 +0900

    Update fortune.js

--- api/fortune.js
+++ api/fortune.js
@@ -1,3 +1,4 @@
+const fetch = require('node-fetch');
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
